45c72f98f14ca097ddf4efb1642dae48eb19e9a8
JordanMillett <55707792+JordanMillett@users.noreply.github.com>
2024-11-06 01:35:39 +0000

    Deploying to gh-pages from @ JordanMillett/Vetra@07c808b3b56bb485c1018f064e5a919a2ff8ec00 🚀

--- js/notifications.js
+++ js/notifications.js
@@ -1,12 +1,32 @@
+async function isServerOnline()
+{   
+    
+    try
+    {
+        const response = await fetch("https://mole-factual-pleasantly.ngrok-free.app/api/pulse", {
+            method: 'GET',
+            headers: {
+                'ngrok-skip-browser-warning': 'true'
+            }
+        });
+        
+        return response.status === 225;
+
+    } catch
+    {
+        
+        console.log("VetraHub is offline");
+        return false;
+    }
+    
+}
+
 async function loadServiceWorker()
 {
-    if ('serviceWorker' in navigator) {
-        window.addEventListener('load', () => {
-            navigator.serviceWorker.register('service-worker.js')
-                .then(registration => {
-                    console.log('Service Worker Registered.');
-                })
-        });
+    if ('serviceWorker' in navigator)
+    {
+        navigator.serviceWorker.register('service-worker.js');
+        console.log('Service Worker Registered.');
     }
 }
 
@@ -17,40 +37,62 @@ function setupMessageListener(dotnetHelper) {
     });
 }
 
-async function initializePushNotifications()
+async function askForNotifications()
 {
-    // Check if service workers and push messaging are supported
     if ('serviceWorker' in navigator && 'PushManager' in window)
     {
-        // Register the service worker
-        const registration = await navigator.serviceWorker.register('service-worker.js');
-
-        // Ask for permission to send notifications
         const permission = await Notification.requestPermission();
         
-        if (permission === 'granted') {
-            // Subscribe the user to push notifications
-            const subscription = await registration.pushManager.subscribe({
-                userVisibleOnly: true,
-                applicationServerKey: 'BBIS4Xi3mCa3vxm7ymp5TWVgYhlTEMgf7vr1cn1eMsz3akiJ1WLIvA538FyJN4tTtBqKrTmu0t6McyZ6ISnY_qQ' // replace with your actual VAPID public key
-            });
-            
-            // Extract the keys
-            const p256dh = subscription.getKey('p256dh');
-            const auth = subscription.getKey('auth');
-            
-            const p256dhBase64 = btoa(String.fromCharCode(...new Uint8Array(p256dh)));
-            const authBase64 = btoa(String.fromCharCode(...new Uint8Array(auth)));
-            
-            const payload = {
-                endpoint: subscription.endpoint,
-                p256dh: p256dhBase64,
-                auth: authBase64
-            };
+        if (permission === 'granted')
+        {
+            console.log('Push notifications permission granted.');
+            return true;
+        } else
+        {
+            console.log('Push notifications permission denied.');
+            return false;
+        }
+    } else
+    {
+        console.log('Push messaging is not supported.');
+        return false;
+    }
+}
 
-            try {
-                // Send the subscription details to your server
-                const response = await fetch('https://mole-factual-pleasantly.ngrok-free.app/api/notifications/subscribe', {
+async function updateNotificationStatus(staySubscribed)
+{
+    if ('serviceWorker' in navigator && 'PushManager' in window)
+    {
+        const registration = await navigator.serviceWorker.register('service-worker.js');
+
+        const subscription = await registration.pushManager.subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: 'BBIS4Xi3mCa3vxm7ymp5TWVgYhlTEMgf7vr1cn1eMsz3akiJ1WLIvA538FyJN4tTtBqKrTmu0t6McyZ6ISnY_qQ' // replace with your actual VAPID public key
+        });
+        
+        const p256dh = subscription.getKey('p256dh');
+        const auth = subscription.getKey('auth');
+        
+        const p256dhBase64 = btoa(String.fromCharCode(...new Uint8Array(p256dh)));
+        const authBase64 = btoa(String.fromCharCode(...new Uint8Array(auth)));
+        
+        const payload = {
+            endpoint: subscription.endpoint,
+            p256dh: p256dhBase64,
+            auth: authBase64
+        };
+        
+        console.log(await isServerOnline());
+        
+        if (await isServerOnline())
+        {
+            try
+            {
+                url = staySubscribed ?
+                    "https://mole-factual-pleasantly.ngrok-free.app/api/notifications/subscribe" :
+                    "https://mole-factual-pleasantly.ngrok-free.app/api/notifications/unsubscribe";
+                
+                const response = await fetch(url, {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json'
@@ -58,20 +100,15 @@ async function initializePushNotifications()
                     body: JSON.stringify(payload)
                 });
         
-                // Check if the response is successful
                 if (response.ok) {
-                    console.log('Subscription successful:', await response.json());
+                    console.log('Subscription update successful:', await response.json());
                 } else {
-                    console.error('Subscription failed with status:', response.status, await response.text());
+                    console.error('Subscription update failed with status:', response.status, await response.text());
                 }
-            } catch (error) {
-                console.error('Error during subscription:', error);
+            } catch (error)
+            {
+                console.error('Error during subscription update:', error);
             }
-            
-        } else {
-            console.log('Push notifications permission denied.');
         }
-    } else {
-        console.log('Push messaging is not supported.');
     }
 }
